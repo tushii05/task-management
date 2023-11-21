1f5913faa0e6edf94a154222372c7aef51138026
Tushar Deshmukh <104415590+tushii05@users.noreply.github.com>
2023-11-21 15:35:41 +0530

    Update user.js  Authentication

--- _middleware/user.js
+++ _middleware/user.js
@@ -15,3 +15,25 @@ function requireToken(req, res, next) {
 }
 
 module.exports = requireToken;
+
+
+
+
+//-------------------Another Options 
+
+function authenticateToken(req, res, next) {
+  const token1 = req.header("Authorization");
+  const token = token1.split(" ")[1];
+
+  console.log(token.split(" ")[1]);
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorised" });
+  }
+  try {
+    const decoded = jwt.verify(token, SECRETSTRING);
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    res.status(400).json({ message: "Invalid token." });
+  }
+}
